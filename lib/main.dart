aac621d6793f2225c19793bc065e255d5accb8d6
TrungNK <nguyenkientrung0412@gmail.com>
2024-04-10 00:30:43 +0700

    list contact ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,58 +26,157 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
+    var contacts = List.generate(
+      100,
+      (index) => ItemContact(
+        showFirstChar: index % 3 == 0,
       ),
+    );
+
+    return Scaffold(
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 160),
-          child: ListView(
-            children: const [
-              ItemNote(),
-              SizedBox(height: 30,),
-              ItemNote(),
-            ],
-          ),
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(120),
+                      border: Border.all(
+                          color: Colors.grey, style: BorderStyle.solid)),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(children: [
+                    const Icon(Icons.menu),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    const Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search for people',
+                          hintStyle: TextStyle(color: Colors.grey),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Container(
+                      width: 32,
+                      height: 32,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'CE',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    )
+                  ]),
+                )),
+            const SizedBox(
+              height: 24,
+            ),
+            Expanded(
+              child: ListView.separated(
+                itemBuilder: (BuildContext context, int index) {
+                  return contacts[index];
+                },
+                separatorBuilder: (BuildContext context, int index) {
+                  return const SizedBox(height: 16);
+                },
+                itemCount: contacts.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        backgroundColor: Colors.blue,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
         ),
       ),
     );
   }
 }
 
-class ItemNote extends StatelessWidget {
-  const ItemNote({super.key});
+class ItemContact extends StatelessWidget {
+  const ItemContact({super.key, this.showFirstChar = false});
+
+  final bool showFirstChar;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
-      child: const Column(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.only(bottom: 12),
-            child: Row(
+          Text(
+            'J',
+            style: TextStyle(
+                fontSize: 24,
+                color: Colors.black.withOpacity(
+                  showFirstChar ? 0.6 : 0,
+                ),
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            width: 32,
+          ),
+          ClipOval(
+            child: SizedBox.square(
+              dimension: 50,
+              child: Image.network(
+                'https://scontent.fhan14-2.fna.fbcdn.net/v/t39.30808-6/429765691_1509923076240496_3227231094154993601_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEUva0bUWKd3UA4zhnoN-ThnIUBhabpi7achQGFpumLthpNZQwLjW84ZBEwZn5NUiyut_oCWTLVN1rq5qoFRxMW&_nc_ohc=3hPHNNkxXagAb6g9xG4&_nc_ht=scontent.fhan14-2.fna&oh=00_AfBWfzqck4oOdWtF6_MnBL20Fr61JyW40HeWfHMNMAkFHA&oe=661B4911',
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 16,
+          ),
+          const Expanded(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Expanded(
-                  child: Text(
-                    'Tiêu đề hjh jkasjdh jaskhd jkashdjashd jkashdj khasjkd hjk hjasdjasjdasdasdada uhdajkhdjkhakjsdhakjsdhkj',
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          'Nguyen Trung',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Text(
-                  'Tiêu đề',
-                  textAlign: TextAlign.end,
-                ),
+                Text('Nội dung'),
               ],
             ),
           ),
-          Text('Nội dung'),
         ],
       ),
     );
   }
 }
-
-
